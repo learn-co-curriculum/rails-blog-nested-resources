46895801f03ac0854948ec056652a1aab6904a61
ahimmelstoss <ahimmelstoss@gmail.com>
2014-05-22 15:45:15 -0400

    ready to go

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   has_many :posts
-  has_many :comments
 
   validates_uniqueness_of :name
   validates_presence_of :name
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ RailsBlog::Application.routes.draw do
 
   resources :users
   resources :tags
-  resources :posts do 
-    resources :comments, :only => [:new, :create, :edit, :destroy]
-  end
+  resources :posts
 
 end
