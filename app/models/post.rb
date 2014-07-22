955530f3e93072e22ee35e65985ebd2f913c6580
ahimmelstoss <ahimmelstoss@gmail.com>
2014-07-22 09:55:47 -0400

    bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  has_many :comments, :dependent => :destroy
   has_many :post_tags
   has_many :tags, :through => :post_tags
-  accepts_nested_attributes_for :tags
+  accepts_nested_attributes_for :tags, reject_if: lambda {|attributes| attributes['name'].blank?}
   validates_presence_of :name, :content
 end
