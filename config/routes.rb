cc46351c5975fb9fae4499f01e34e59a1457b320
maxwellbenton <maxwellbenton@gmail.com>
2019-02-13 19:22:44 -0500

    rails app:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
-RailsBlog::Application.routes.draw do
-
-  resources :users
-  resources :tags
-  resources :posts
-
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
